0fdbe950ced5f49c0b6ec48cf59b224727e4cf2c
Clifford Myrtil <cliffordhamyrtil@gmail.com>
2023-09-05 15:32:05 -0400

    Completed exercise 2

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -8,25 +9,53 @@ namespace ArraysAndLists
         {
             //TODO:
 
+            //DONE
             // Create an int Array and populate numbers 1-10
 
+            var numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //DONE
             /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            * Name one List "evens"
+            * Name the other List "odds"
+            */
 
-            /* Using either a foreach or for loop,
-             * nest an if statement to check to see
-             *  if a number is even or odd.
-             * Then add those numbers to either the evens List
-             * or the odds List
-             */
+            var evens = new List<int>();
+            var odds = new List<int>();
 
+            //DONE
+            //           /* Using either a foreach or for loop,
+            //* nest an if statement to check to see
+            //*  if a number is even or odd.
+            //* Then add those numbers to either the evens List
+            //* or the odds List
+            //*/
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                    Console.WriteLine($"{num} is an even number");
+                }
+                else
+                    odds.Add(num);
+                    Console.WriteLine($"{num} is an odd number");
+
+            }
+            //DONE
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+
+            Console.WriteLine("\nNow lets group them in order");
+            foreach (var oddItem in odds)
+            Console.WriteLine($"{oddItem} is odd, but not weird");
+            foreach (var item in evens)
+            Console.WriteLine($"{item} are even, but not steven");
+
         }
     }
 }
